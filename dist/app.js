9f39d573c02bbcaef7cfb9d8eb5923c91e25c51f
Abdul Rehman <s.abderrehman@gmail.com>
2019-07-27 23:15:07 +0500

    Add Authorization module with middleware protection and protected routes.

--- dist/app.js
+++ dist/app.js
@@ -2,10 +2,42 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-app.get('/', (req, res, next) => {
+const jwt = __importStar(require("jsonwebtoken"));
+exports.app = express_1.default();
+const verifyToken = (req, res, next) => {
+    const bearerHeader = req.headers['Authorization'];
+    console.log(bearerHeader);
+    if (typeof bearerHeader !== undefined) {
+        console.log('Go a Head');
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(403);
+    }
+};
+exports.app.get('/api', (req, res, next) => {
     res.send("It's Works");
 });
-app.listen(5000, () => console.log("Hi There, It's me 5000"));
+exports.app.post('/api/post', verifyToken, (req, res, next) => {
+    res.send('Post Created!!!');
+});
+exports.app.post('/api/login', (req, res, next) => {
+    const user = {
+        username: 'abdulrehman',
+        email: 'ab@gmail.com',
+    };
+    jwt.sign({ user: user }, 'secretkey', (err, token) => {
+        console.log(err, token);
+        res.json({ token: token });
+    });
+});
+exports.app.listen(5000, () => console.log("Hi There, It's me 5000"));
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,52 @@
 import express, { Application, Response, Request, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
+export const app: Application = express();
 
-const app: Application = express();
-
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+	const bearerHeader: any = req.headers['authorization'];
+	if (bearerHeader !== undefined) {
+		const bearer = bearerHeader.split(' ');
+		const bearerToken = bearer[1];
+		req.body = {
+			token: bearerToken,
+		};
+		next();
+	} else {
+		res.send({
+			status: 403,
+			message: 'Authorization Error!',
+		});
+	}
+};
+app.get('/api', (req: Request, res: Response, next: NextFunction) => {
 	res.send("It's Works");
 });
+
+app.post('/api/post', verifyToken, (req: Request, res: Response, next: NextFunction) => {
+	jwt.verify(req.body.token, 'secretkey', (err: any, authData: any) => {
+		if (err) {
+			res.send({
+				status: 403,
+				message: 'Authorization Error!',
+			});
+		} else {
+			res.json({
+				message: 'Post created!',
+				authData,
+			});
+		}
+	});
+});
+
+app.post('/api/login', (req: Request, res: Response, next: NextFunction) => {
+	const user = {
+		username: 'abdulrehman',
+		email: 'ab@gmail.com',
+	};
+	jwt.sign({ user: user }, 'secretkey', { expiresIn: '20s' }, (err: any, token: any) => {
+		console.log(err, token);
+		res.json({ token: token });
+	});
+});
+
 app.listen(5000, () => console.log("Hi There, It's me 5000"));
